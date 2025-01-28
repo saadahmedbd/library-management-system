7d1caf9944c996f0a8de8fae1cfca76e6d281a18
saadahmedbd <saadahmedbd0@gmail.com>
2025-01-28 08:25:54 +0600

    update security data

--- src/main/java/com/saad/library/management/model/Role.java
+++ src/main/java/com/saad/library/management/model/Role.java
@@ -1,15 +1,56 @@
 package com.saad.library.management.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
+
+
 public class Role {
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     private long id;
     private String name;
+
     @ManyToMany(mappedBy = "roles")
-    private Set<security_User> users;
+    private Set<security_User> users =new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(long id, String name, Set<security_User> users) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<security_User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<security_User> users) {
+        this.users = users;
+    }
 }
--- src/main/java/com/saad/library/management/model/security_User.java
+++ src/main/java/com/saad/library/management/model/security_User.java
@@ -1,10 +1,17 @@
 package com.saad.library.management.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class security_User {
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
@@ -12,12 +19,43 @@ public class security_User {
     private String username;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(cascade =CascadeType.PERSIST)
     @JoinTable(name =
             "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    private Set<Role> roles;
+    private Set<Role> roles =new HashSet<>();
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
